b43eb30981b8dd523e14f7826ca8d21970908c02
Vorobiov Vlad <vorobyov.vladislav@gmail.com>
2020-04-20 15:54:53 +0300

    initial commit

---
+++ concat.test.js
@@ -0,0 +1,19 @@
+// import concat from './concat'
+const myConcat = require('./concat')
+String.prototype.myConcat = myConcat
+
+test('concat a & b chars to \'ab\'', () => {
+    const a = 'a'
+    const b = 'b'
+    expect(a.myConcat(b)).toBe('ab')
+})
+
+test('Concat multiply string correctly', () => {
+    const a = 'a'
+    expect(a.myConcat('bb', 'string', 'l,')).toBe('abbstringl,')
+})
+
+test('Concat with object and array correct', () => {
+    const a = 'a'
+    expect(a.myConcat([1,2,3], {a: 7})).toBe(a.concat([1,2,3], {a:7}))
+})
