8198a3a56b99e547e60ee1972c7886229d41030c
Vorobiov Vlad <vorobyov.vladislav@gmail.com>
2020-04-20 23:00:05 +0300

    add checkout to existing branch. fix readme.md

--- readme.md
+++ readme.md
@@ -4,7 +4,7 @@ run `npm start` and input your github name
 
 please note that you alraedy on your own git branch with name `yourName-string-methods`  
 find folder with your name  
-implement all methods in there files  
+implement all methods in files with corresponding names  
 run `npm test`
 
 if all tests are passed commit your changes, push them and create PR
--- start.js
+++ start.js
@@ -10,16 +10,18 @@ const rl = readline.createInterface({
     output: process.stdout
 })
 
-rl.question("Please enter your github name: ", function(nameFromInput) {
-    exec(`git checkout -b ${nameFromInput}-string-methods`, (err) => {if(err) throw err})
+rl.question("Please enter your github name: ", async function(nameFromInput) {
     const dir = `./${nameFromInput}`;
-
     if (!fs.existsSync(dir)){
+        exec(`git checkout -D ${nameFromInput}-string-methods`, (err) => {console.log(err); if(err) throw err })
+        exec(`git checkout -b ${nameFromInput}-string-methods`, (err) => {console.log(err); if(err) throw err })
         fs.mkdirSync(dir);
+        fs.copyFile('./concat.example.js', `${nameFromInput}/concat.js`, (error) => {if(error) throw error} )
+        const fd = fs.openSync('./name.js', 'w')
+        fs.writeFile(fd, `module.exports = '${nameFromInput}'`, 'utf8', function(){})
+    } else {
+        console.log(`You already have the folder: ${nameFromInput}. Please checkout to your branch: ${nameFromInput}-string-methods or delete your folder and run npm start again.`)
     }
-    const fd = fs.openSync('./name.js', 'w')
-    fs.writeFile(fd, `module.exports = '${nameFromInput}'`, 'utf8', function(){})
-    fs.copyFile('./concat.example.js', `${nameFromInput}/concat.js`, (error) => {if(error) throw error} )
     rl.close()
 
 })
